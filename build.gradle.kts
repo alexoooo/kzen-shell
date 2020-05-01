ef011b2ba87ea63c544f6e27289656a6c451fd67
alexoooo <alex@ostrovsky.biz>
2020-04-30 20:27:19 -0400

    release 0.13.0, upgraded to Gradle 6.3 with Kotlin DSL and language version 1.4

---
+++ build.gradle.kts
@@ -0,0 +1,55 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import org.springframework.boot.gradle.tasks.bundling.BootJar
+import org.jetbrains.kotlin.gradle.targets.js.webpack.KotlinWebpack
+
+
+plugins {
+    kotlin("jvm") version kotlinVersion
+    kotlin("plugin.spring") version kotlinVersion
+    id("org.springframework.boot") version springBootVersion
+    id("io.spring.dependency-management") version dependencyManagementVersion
+}
+
+
+group = "tech.kzen"
+version = "0.13.0"
+
+
+repositories {
+    maven("https://dl.bintray.com/kotlin/kotlin-eap")
+    mavenCentral()
+}
+
+
+dependencies {
+    implementation(kotlin("stdlib-jdk8"))
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.springframework.boot:spring-boot-starter-webflux")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+
+//    compile('org.springframework.boot:spring-boot-starter-web')
+
+    implementation("com.google.guava:guava:$guavaVersion")
+    implementation("io.github.microutils:kotlin-logging:$kotlinLogging")
+
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("io.projectreactor:reactor-test")
+}
+
+
+tasks {
+    compileKotlin {
+        kotlinOptions {
+            freeCompilerArgs = listOf("-Xjsr305=strict")
+            jvmTarget = jvmTargetVersion
+        }
+    }
+//    compileTestKotlin {
+//        kotlinOptions.jvmTarget = jvmTargetVersion
+//    }
+}
+
+
+tasks.getByName<BootJar>("bootJar") {
+    archiveClassifier.set("boot")
+}
---
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -0,0 +1,13 @@
+const val kotlinVersion = "1.4-M1"
+//const val kotlinVersion = "1.3.72"
+
+const val jvmTargetVersion = "13"
+
+const val springBootVersion = "2.2.6.RELEASE"
+const val dependencyManagementVersion = "1.0.9.RELEASE"
+
+const val coroutinesVersion = "1.3.5"
+
+const val guavaVersion = "29.0-jre"
+
+const val kotlinLogging = "1.7.8"
