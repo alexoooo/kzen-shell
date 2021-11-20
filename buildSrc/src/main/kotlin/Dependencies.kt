c95732c06e229ba2ee2a907721b1169a4dc0a7e3
alexoooo <alex@ostrovsky.biz>
2021-11-20 16:57:52 -0500

    release 0.24.1

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -1,7 +1,7 @@
-const val kotlinVersion = "1.5.31"
-const val jvmTargetVersion = "16"
-const val jvmToolchainVersion = 16
-const val springBootVersion = "2.5.5"
+const val kotlinVersion = "1.6.0"
+const val jvmTargetVersion = "17"
+const val jvmToolchainVersion = 17
+const val springBootVersion = "2.6.0"
 const val dependencyManagementVersion = "1.0.11.RELEASE"
 
 const val guavaVersion = "31.0.1-jre"
