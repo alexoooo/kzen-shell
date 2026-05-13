1863b95ef6455e1d0cae691ed480798abc41d60b
alexoooo <alex@ostrovsky.biz>
2026-05-12 20:51:16 -0400

    minor bugfix progress

--- src/main/kotlin/tech/kzen/shell/KzenShellMain.kt
+++ src/main/kotlin/tech/kzen/shell/KzenShellMain.kt
@@ -9,7 +9,6 @@ import io.ktor.server.netty.*
 import io.ktor.server.plugins.contentnegotiation.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
-import io.ktor.util.pipeline.*
 import tech.kzen.shell.context.KzenShellContext
 import tech.kzen.shell.context.KzenShellProperties
 import tech.kzen.shell.ui.DesktopUi
