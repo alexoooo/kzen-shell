0345ae23c23f8a249d0e94127e71d0afc3c773b4
alexoooo <alex@ostrovsky.biz>
2026-07-07 23:33:04 -0400

    dependency update

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -1,9 +1,9 @@
 const val kotlinVersion = "2.4.0"
-const val jvmTargetVersion = "25"
-const val javaVersion = 25
-const val jvmToolchainVersion = 25
-const val ktorVersion = "3.5.0"
+const val jvmTargetVersion = "26"
+const val javaVersion = 26
+const val jvmToolchainVersion = 26
+const val ktorVersion = "3.5.1"
 
 const val guavaVersion = "33.6.0-jre"
-const val logbackVersion = "1.5.34"
+const val logbackVersion = "1.5.37"
 const val jacksonModuleKotlin = "3.2.0"
